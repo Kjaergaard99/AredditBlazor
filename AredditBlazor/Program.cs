f025198b7d9c5054a0c62bdeddf470ecb743e4a0
Mads Kjaergaard <madskj2010@live.dk>
2022-10-26 09:30:11 +0200

    ahahah

--- AredditBlazor/Program.cs
+++ AredditBlazor/Program.cs
@@ -9,3 +9,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+// simpel ændring daniel ikke kan finde ud af -- trash cunt
