6a8bedaee6d70ff93a5758b1c4ec32ac0fa62c0a
danieldawsonjensen <dani540c@gmail.com>
2022-10-26 10:30:00 +0200

    fortsat opsætning v2

--- AredditBlazor/Program.cs
+++ AredditBlazor/Program.cs
@@ -1,11 +1,15 @@
-using AredditBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Model;
+using Services;
+using AredditBlazor;
+using Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ApiService>();
 
 await builder.Build().RunAsync();
